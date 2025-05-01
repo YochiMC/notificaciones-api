092c136d6132310febb7d005d3ef3f9fa186f81e
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-05-01 00:04:01 -0600

    final

--- src/firebase.js
+++ src/firebase.js
@@ -2,26 +2,16 @@
 Es necesaria la clave de la base de datos y crear el respectivo archivo*/
 
 require('dotenv').config();
-const path = require('path');
-const admin = require('firebase-admin');
-const { getFirestore } = require('firebase-admin/firestore');
 
-// Obtiene la ruta desde la variable de entorno o usa un valor por defecto
-const firebaseKeyPath = process.env.FIREBASE_KEY_PATH || 'notificaciones-api-f6991-firebase-adminsdk-fbsvc-f02bea3e9a';
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore')
 
-// Construye la ruta absoluta al archivo JSON desde la raíz del proyecto
-const serviceAccountPath = path.resolve(__dirname, firebaseKeyPath);
-const serviceAccount = require(serviceAccountPath);
-
-// Inicializa Firebase con la credencial
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+initializeApp({
+    credential: applicationDefault()
 });
 
-// Obtiene la instancia de Firestore
 const db = getFirestore();
 
-// Exporta la base de datos para usar en otros archivos
 module.exports = {
-  db
+    db
 };
